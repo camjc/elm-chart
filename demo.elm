7fbc63c6f5918d52c12494a0bace4009e9da3892
Cameron Chamberlain <hi@camjc.com>
2016-03-14 17:12:18 +1100

    Build: Match elm style guide

--- demo.elm
+++ demo.elm
@@ -1,7 +1,9 @@
+module Main (main) where
+
 import Chart
 import Svg
 
 
 main : Svg.Svg
 main =
-  Chart.pie 300 [{color = "#0ff", value = 3}, {color = "purple", value = 27}]
+  Chart.pie 300 [ { color = "#0ff", value = 3 }, { color = "purple", value = 27 } ]
